41752d32e0ebf935ae0514b486a78dc012a07f52
Simon Seyock <simonseyock@gmail.com>
2015-03-09 23:56:40 +0100

    Clock

--- javascript/Clock.js
+++ javascript/Clock.js
@@ -3,8 +3,11 @@
 
 // #include "init.js"
 // #include "StateExchangeObject.var.js"
+// #include "EventHandling.js"
 
 synth.Clock = function (audioContext) {
+	synth.EventHandling.call(this);
+	
 	this.setBpm(120);
 	//this.on("statechange:bpm", ...);
 	this.setBeats(4);
@@ -13,14 +16,21 @@ synth.Clock = function (audioContext) {
 	this.audioContext_ = audioContext;
 	
 	this.bufferTime_ = 0.05;
+	this.precognitionTime_ = 0.1;
+	this.gridTime_ = 0.2;
 	
 	this.startTime_ = 0;
 	this.started_ = false;
 	
-	this.players_ = [];
-	this.informer_ = null;
+	this.tickerI_ = null;
+	this.tickerT_ = null;
+	
+	this.registerEventType("tick"); // gives relative times to startTime
+	this.registerEventType("start");
+	this.registerEventType("stop");
 };
 synth.inherits(synth.Clock, synth.StateExchangeObject);
+synth.inherits(synth.Clock, synth.EventHandling);
 
 synth.Clock.prototype.setBpm = function (bpm) {
 	this.bpm_ = bpm;
@@ -38,32 +48,50 @@ synth.Clock.prototype.setBeats = function (beats) {
 };
 
 synth.Clock.prototype.start = function (/* time here? */) {
+	
+	this.fireEvent("start", [0]);
+	
+	this.fireEvent("tick", [ 0, this.gridTime_ ] );
+	this.tickCount_ = 1;
+	
 	var that = this;
-	this.informer_ = setInterval( function () {
-		that.informPlayers(that.audioContext_.currentTime + that.bufferTime_);
-	}, this.getBarLength() * 1000);
+	
+	that.tickerT_ = setTimeout( function () {
+		that.fireEvent("tick", [that.gridTime_, 2* that.gridTime_]);
+		that.tickCount_++;
+		that.tickerI_ = setInterval( function () {
+			that.fireEvent("tick", [that.gridTime_ * that.tickCount_, that.gridTime_ * (that.tickCount_ + 1)]);
+			that.tickCount_++;
+		}, that.gridTime_ * 1000);
+	}, (this.gridTime_-this.precognitionTime_) * 1000);
 	
 	this.started = true;
-	this.startTime_ = this.audioContext_.currentTime + this.bufferTime_;
-	
-	this.informPlayers(this.audioContext_.currentTime + this.bufferTime_);
+	this.startTime_ = this.audioContext_.currentTime;
 };
 
 synth.Clock.prototype.stop = function (/* time here? */) {
-	clearInterval(this.informer_);
+	//clearInterval(this.informer_);
 	this.started = false;
-	// fire stop
+	
+	clearTimeout(this.tickerT_);
+	clearInterval(this.tickerI_);
+	
+	this.fireEvent("stop", [this.audioContext_.currentTime - this.startTime_]);
 };
 
-synth.Clock.prototype.registerPlayer = function (player) {
-	this.players_.push(player);
+synth.Clock.prototype.getRealTime = function (time) {
+	return this.startTime_ + this.bufferTime_ + time;
 };
 
-synth.Clock.prototype.informPlayers = function (when) {
-	for (var i=0; i<this.players_.length; i++) {
-		this.players_[i].playBar(when, this.getBarLength());
-	}
-};
+// synth.Clock.prototype.registerPlayer = function (player) {
+	// this.players_.push(player);
+// };
+
+// synth.Clock.prototype.informPlayers = function (when) {
+	// for (var i=0; i<this.players_.length; i++) {
+		// this.players_[i].playBar(when, this.getBarLength());
+	// }
+// };
 
 // synth.Clock.prototype.bars2beats = function (bars) {
 	
