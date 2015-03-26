67f9e7703934bfd122ce5e954debedfb76d8c112
Simon Seyock <simonseyock@gmail.com>
2015-03-26 09:49:31 +0100

    Added play and stop colors

--- javascript/viewcontrollers/PlayBackViewController.js
+++ javascript/viewcontrollers/PlayBackViewController.js
@@ -12,32 +12,54 @@ synth.viewController.Playback = function (clock, opt_options) {
 	this.clock_ = clock;
 
 	this.className_ = opt_options.className || "synth-playback";
+  this.classNameActive_ = "synth-active";
 
 	synth.viewController.ViewController.call(this, opt_options);
 
-	var $playButton = $("<button>").addClass(this.className_ + "-play").append(synth.svgs.play);
+  var $playButton, $stopButton, $bpmDiv;
+
+  // PLAY
+	$playButton = $("<button>").addClass(this.className_ + "-play").append(synth.svgs.play);
 	$playButton.on("click", function () {
 		this.clock_.start();
 	}.bind(this));
 	this.$element_.append($playButton);
 
-	var $stopButton = $("<button>").addClass(this.className_ + "-stop").append(synth.svgs.stop);
+  this.clock_.on("start", function () {
+    $playButton.addClass(this.classNameActive_);
+    $stopButton.removeClass(this.classNameActive_);
+  }.bind(this));
+
+  //STOP
+	$stopButton = $("<button>").addClass(this.className_ + "-stop").append(synth.svgs.stop);
+  $stopButton.addClass(this.classNameActive_);
 	$stopButton.on("click", function () {
 		this.clock_.stop();
 	}.bind(this));
 	this.$element_.append($stopButton);
 
-	var $bpmDiv = $("<div>").addClass(this.className_ + "-bpm");
+  this.clock_.on("stop", function () {
+    $stopButton.addClass(this.classNameActive_);
+    $playButton.removeClass(this.classNameActive_);
+  }.bind(this));
+
+  //BPM
+	$bpmDiv = $("<div>").addClass(this.className_ + "-bpm");
 	var bpmField = new synth.html.InputFieldWithValueDrag(0.7);
 	bpmField.setValue(this.clock_.getBpM());
 
 	bpmField.on("input", function () {
 		this.clock_.setBpM(parseFloat(bpmField.getValue()));
-		bpmField.setValue(this.clock_.getBpM());
 	}.bind(this));
+
+  this.clock_.on("tempoChange", function () {
+    bpmField.setValue(this.clock_.getBpM());
+  }.bind(this));
 
 	$bpmDiv.append(bpmField.get$Element()).append("BpM");
 	this.$element_.append($bpmDiv);
+
+  //
 };
 synth.inherits(synth.viewController.Playback, synth.viewController.ViewController);
 // #endif
